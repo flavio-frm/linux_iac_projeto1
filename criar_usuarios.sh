65d5b0e33597c30a8ab5002d991d7f27f86d5b10
flavio-frm <rmachado.flavio@gmail.com>
2022-10-27 17:41:44 -0300

    Arquivos de Script - Curso de Linux - V_1.0

--- criar_usuarios.sh
+++ criar_usuarios.sh
@@ -14,46 +14,42 @@ echo "-------------------------------"
 # Criando os grupos de usuarios
 #
 echo "**** Criando Grupos ****"
-groupadd GRP_ADM
-groupadd GRP_VEN
-groupadd GRP_SEC
+groupadd GRP_ADM GRP_VEN GRP_SEC
 
 echo "-------------------------------"
-
-
 # Criando os usuarios
 #
 echo "**** Criando Usuarios ****"
 
-useradd carlos -c "Carlos Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd carlos -c "Carlos Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
 passwd carlos -e
 
-useradd maria -c "Maria Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd maria -c "Maria Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
 passwd maria -e
 
-useradd joao -c "João Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd joao -c "João Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
 passwd joao -e
 #
 #
 #
-useradd debora -c "Debora Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd debora -c "Debora Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_VEN
 passwd debora -e
 
-useradd sebastiana -c "Sebastiana Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_VEN
 passwd sebastiana -e
 
-useradd roberto -c "Roberto Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd roberto -c "Roberto Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_VEN
 passwd roberto -e
 #
 #
 #
-useradd josefina -c "Josefina Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd josefina -c "Josefina Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 passwd josefina -e
 
-useradd amanda -c "Amanda Salles" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd amanda -c "Amanda Salles" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 passwd amanda -e
 
-useradd rogerio -c "Rogerio Fulano" -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd rogerio -c "Rogerio Fulano" -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 passwd rogerio -e
 
 echo "-------------------------------"
@@ -64,19 +60,21 @@ echo "**** Aplicando Permissões ****"
 cd /
 chmod 777 /publico/
 
-chown root:GRP_ADM /adm/
+chown root:GRP_ADM /adm
 chmod 770 /adm
 
-chown root:GRP_VEN /ven/
+chown root:GRP_VEN /ven
 chmod 770 /ven
 
-chown root:GRP_SEC /sec/
+chown root:GRP_SEC /sec
 chmod 770 /sec
 
 echo "**** Script finalizado ****"
-echo " "
+
 echo "os usuarios foram criados e devem alterar sua senha no 1º logon"
 echo "senha padrão dos usuarios criados é:"
 echo "senha123"
+
+
 
 
